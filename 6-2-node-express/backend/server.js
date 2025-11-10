e2f889f779206c60aff8c7ba3496408df7fea60f
reyyynad <s202276760@kfupm.edu.sa>
2025-11-10 11:38:23 +0300

    Done todo 5

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -5,15 +5,21 @@ import { getRandomQuote } from "./quotes.js";
 const app = express();
 const PORT = 3000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
 
 
 app.use(cors());
 
 
-{/*write code to define routes*/}
+app.get("/", (req, res) => {
+    res.send("Welcome to the Quote Generator API");
+  });
 
 
-{/*write code to create server*/}
+app.get("/api/quote", (req, res) => {
+    const quote = getRandomQuote();
+    res.json({ quote });
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
