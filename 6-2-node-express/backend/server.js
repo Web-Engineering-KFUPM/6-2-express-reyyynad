8d813842a15d588a8bccd9eec918ecc0359c85ad
reyyynad <s202276760@kfupm.edu.sa>
2025-11-10 11:33:40 +0300

    Done todo3

--- 6-2-node-express/backend/server.js
+++ 6-2-node-express/backend/server.js
@@ -2,6 +2,13 @@ import express from "express";
 import cors from "cors";
 import { getRandomQuote } from "./quotes.js";
 
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
+
 {/*write code for cors*/}
 
 
